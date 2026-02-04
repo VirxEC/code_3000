6eda3fc42da880f50fe17d78edbce17caafd1a49
VirxEC <virx@virxcase.dev>
2026-02-04 15:56:19 -0500

    Optimize params

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -1,21 +1,21 @@
 # packages
-import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
 
 # set seed
 seed = 314
+
 
 def train_model(X, y, seed=seed):
     """
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
-        max_depth=8,
+        learning_rate=0.0025,
+        n_estimators=1400,
+        max_depth=2,
         subsample=1,
         min_samples_leaf=1,
-        random_state=seed
+        random_state=seed,
     )
     model.fit(X, y)
     return model
